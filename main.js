2cab28b89f42360ca55f32a67a2320b1f2d277f0
mattia0419 <pironemattia6@gmail.com>
2023-08-29 16:14:06 +0200

    add img

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            title : "HELLO WORLD!!"
+            title : "HELLO WORLD!!",
+            img : "01.webp",
         };
     }
 }).mount("#app")
